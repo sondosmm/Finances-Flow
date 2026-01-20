834c244f62144c4cd6eb5e3c4d8733e92a10b2b1
sondosmm <sondosmm6@gmail.com>
2026-01-20 18:35:34 +0200

    add the auth and profile controllers

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,26 @@
 const User =require ('../models/userModel');
 const ApiError = require('../utils/apiError');
-const asyncHandler =require('asyncHandler')
+const asyncHandler =require('express-async-handler');
+const bcrypt= require('bcryptjs');
+const generateToken=require('../utils/generateToken')
 exports.register=asyncHandler(async(req,res,next)=>{
-    
+    const {name, email,password}=req.body;
+    const user=await User.create({name,email,password});
+    res.status(201).json({message:'user registered successfully',id:user._id});
+});
+
+exports.login=asyncHandler(async(req,res,next)=>{
+    const {email,password}=req.body;
+    const user =await User.findOne({email}).select('+password');
+    if (!user ||!(await bcrypt.compare(password,user.password))){
+        return next(new ApiError('incorrect email or password',401));
+    }
+    const accessToken = generateToken(user._id);
+    res.cookie("accessToken", accessToken, { httpOnly: true });
+    res.status(200).json({message:'user logged in successfully'});
+});
+
+exports.logout=asyncHandler(async(req,res,next)=>{
+    res.clearCookie("accessToken", { httpOnly: true });
+    res.status(200).json({message:'user logged out successfully'});
 });
---
+++ backend/controllers/userController.js
@@ -0,0 +1,24 @@
+const User= require('../models/userModel');
+const ApiError=require('../utils/apiError');
+const asyncHandler = require("express-async-handler");
+
+
+exports.getUserInfo=asyncHandler(async(req,res,next)=>{
+    const user=await User.findById(req.user._id);
+    res.status(200).json({data:user});
+});
+
+exports.updateUserInfo=asyncHandler(async(req,res,next)=>{
+    const {name,budget}=req.body;
+    //if i used the update method it will be a bit diff i will pass the updates and also new :true and run validation:true
+    //with no need to use user.save the updates will be automatically saved
+    const user = await User.findById(req.user._id);
+    if (name!==undefined)
+        user.name=name;
+    if (budget !== undefined)
+        user.budget = budget;
+    await user.save();
+   res.status(200).json({message:'user updated successfully',data:user});
+});
+
+

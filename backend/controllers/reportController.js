f9562770f4ff45690d1a940b88cf0fe9c2015e6a
sondosmm <sondosmm6@gmail.com>
2026-01-24 23:27:45 +0200

    added report controller

---
+++ backend/controllers/reportController.js
@@ -0,0 +1,16 @@
+const excel = require('../utils/excel.js');
+const reportHelper = require('../utils/reportHelper');
+const fs = require('fs');
+const asyncHandler = require('express-async-handler');
+
+
+exports.getMonthlyExcel = asyncHandler(async (req, res, next) => {
+    const { month, year } = req.query;
+
+    const summary = await reportHelper.getSummary(req.user._id, month * 1, year * 1);
+
+    const filePath = await excel.generateExcel(summary);
+    res.download(filePath
+       // , () => { fs.unlinkSync(filePath) }
+    );
+});
